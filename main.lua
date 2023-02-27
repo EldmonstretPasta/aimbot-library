a8d5a0cfa22e206d671cde5b3c6a10337ab3242f
EldmonstretPasta <89604461+EldmonstretPasta@users.noreply.github.com>
2023-02-27 16:11:02 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,19 @@
 local camera = workspace.Camera
 
-local configs = {
-	outline = false,
-	outlinecolor = Color3.fromRGB(255, 255, 255)
-}
+getgenv().outline = false
+getgenv().outlinecolor = Color3.fromRGB(255,255,255)
 
 while wait() do
 	for i,v in pairs(game.Players:GetPlayers()) do
-		while configs.outline do
+		while getgenv().outline do
 			local highlight = Instance.new("Highlight", v.Character)
-			if configs.outline == true then
+			if getgenv().outline == true then
 				task.wait()
 				highlight.Enabled = true
 				highlight.DepthMode = "AlwaysOnTop"
 				highlight.Name = "Outline"		
 				highlight.FillTransparency = 1
-				highlight.OutlineColor = configs.outlinecolor
+				highlight.OutlineColor = getgenv().outlinecolor
 			else
 				highlight.Enabled = false
 				return
